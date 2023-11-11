03035a8e141fc9467d7838ba72f592a3e7d9c73b
7R <aufinege0156@gmail.com>
2023-11-12 01:33:26 +0800

    Site updated: 2023-11-12 01:33:25

--- js/my_waline.js
+++ js/my_waline.js
@@ -3,23 +3,25 @@ let haedIocns;
 fetch('/assets/emoji/info.json').then((response) => response.json()).then((json) => haedIocns = json['items']);
 
 function testCallBack() {
-    setTimeout(() => {
-        Array.prototype.filter.call(
-            testElements,
-            function (testElement) {
-                if (testElement.getElementsByTagName('img') != null)
-                {
-                    var ele = testElement.getElementsByTagName('img')[0];
-                    if (ele.hasClass('change-end')) { return }
+    Array.prototype.filter.call(
+        testElements,
+        function (testElement) {
+            if (testElement.getElementsByTagName('img') != null)
+            {
+                var ele = testElement.getElementsByTagName('img')[0];
+                if (ele.src.includes("/assets/avatar.png")) {  
                     var rand = Math.floor(Math.random() * haedIocns.length);
                     ele.src = "/assets/emoji/judai_"+ haedIocns[rand] + ".png"
                     ele.filters = 'alpha(opacity=40)';
-                    ele.classList.add('change-end');
-                    // testElement.innerHTML = '<img src="/assets/emoji/judai_'+ haedIocns[rand] +'.png">';
                 }
-            },
-        );
-    }, 2000);    
+                // ele.classList.add('change-end');
+                // testElement.innerHTML = '<img src="/assets/emoji/judai_'+ haedIocns[rand] +'.png">';
+            }
+        },
+    );
 }
 
+// window.addEventListener('DOMContentLoaded', testCallBack());
 setInterval("testCallBack()", "2000");
+
+// /assets/avatar.png
